0d1a4ae6ccdbee29574a238b50f987c5544f3593
rd59140 <rd59140@MKD-CORP-BCP110.CORP.CYIENT.COM>
2021-06-13 22:37:19 +0530

    Data

--- src/test/java/com/cyient/uilites/DataValidUilites.java
+++ src/test/java/com/cyient/uilites/DataValidUilites.java
@@ -39,7 +39,35 @@ public class DataValidUilites {
 		
 		return main;
 	}
-
 	
+	
+	public Object[][] validClData(){
+		
+		Object[][] main=new Object[1][6];
+		
+		main[0][0]= "Email ID";
+		main[0][1]= "Name";
+		main[0][2]= "Mobile Number";
+		main[0][3]= "Pincode";
+		main[0][4]= "Address";
+		main[0][5]= "City";
+		main[0][6]= "State";
+		
+		main[1][0]= "RD@gmail.com";
+		main[1][1]= "Rak";
+		main[1][2]= " ";
+		main[1][3]= "284002";
+		main[1][3]= "45,Fgffgg";
+		main[1][3]= "Agra";
+		main[1][3]= "Uttar Pradesh";
 
+		return main;
+
+}
+	public Object[][] validClExcelData() throws IOException
+	{
+		Object[][] main= ExcelUtilies.getSheetIntoObjectArray("src/test/resources/TestData/Payment Report.xlsx", "validCredentialExcelData"); 
+		
+		return main;
+	}
 }
