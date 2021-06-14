7131f0cd7a15b64be0c8a3ce8d2883ae0a36fd17
rd59140 <rd59140@MKD-CORP-BCP110.CORP.CYIENT.COM>
2021-06-14 14:37:59 +0530

    ScreenShot

--- src/test/java/com/cyient/uilites/ScreenshotUtils.java
+++ src/test/java/com/cyient/uilites/ScreenshotUtils.java
@@ -1,5 +1,21 @@
 package com.cyient.uilites;
+import java.io.File;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 
 public class ScreenshotUtils {
+	public static void takeScreenShot(WebDriver driver)
+	{
+		TakesScreenshot ts=(TakesScreenshot) driver;
+		File file= ts.getScreenshotAs(OutputType.FILE);	
+		Date date=new Date();
+		String sDate=date.toString().replace(":", "-");
+		file.renameTo(new File("screenshot/sc_"+sDate+".png"));
+	}
 
 }
+
+
